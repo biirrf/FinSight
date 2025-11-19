5d395f566c65c9b0d71b95a3b8784dbd0f2050bd
Mohaned <muhanedzaeem@gmail.com>
2025-11-19 16:27:01 +0000

    implemented stock search

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,18 @@
+"use client"
+
+import { use, useCallback, useRef} from "react";
+
+export function  useDebounce(callback: () => void, delay: number) {
+
+    const timeoutRef = useRef<NodeJS.Timeout | null> (null);
+
+    return useCallback(() => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+
+        timeoutRef.current = setTimeout(callback, delay);
+    }, [callback, delay]);
+
+}
+

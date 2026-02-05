4158d436e046d9d20aff2cae4132dc90d066714e
Mohaned <muhanedzaeem@gmail.com>
2026-02-05 13:16:22 +0000

    Add Inngest Cloud webhook route

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,3 +1,10 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/lib/inngest/client";
+import * as functionsModule from "@/lib/inngest/functions";
+
+const functions = Object.values(functionsModule) as any[];
+
+export const { GET, POST, PUT } = serve(inngest, ...functions);
 import { inngest } from "@/lib/inngest/client";
 import { sendDailyNewsSummary, sendSignUpEmail } from "@/lib/inngest/functions";
 import { serve } from "inngest/next";
